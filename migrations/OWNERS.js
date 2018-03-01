546c18e50a55d30d9c5ed396d03b715b6456722a
Kayvon Tehranian <ktehranian@gmail.com>
2018-03-01 12:07:11 -0800

    Add OWNERS file

---
+++ migrations/OWNERS.js
@@ -0,0 +1,11 @@
+const keccak256 = require("js-sha3").keccak_256;
+
+const OWNER_ONE = "0x" + keccak256("test account one");
+const OWNER_TWO = "0x" + keccak256("test account two");
+const OWNER_THREE = "0x" + keccak256("test account three");
+const OWNER_FOUR = "0x" + keccak256("test account four");
+const OWNER_FIVE = "0x" + keccak256("test account five");
+
+const OWNERS = [OWNER_ONE, OWNER_TWO, OWNER_THREE, OWNER_FOUR, OWNER_FIVE];
+
+module.exports.owners = OWNERS;
