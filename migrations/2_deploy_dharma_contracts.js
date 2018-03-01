b8b506d3583fc25d7044b42d5c0131af644cb12d
Kayvon Tehranian <ktehranian@gmail.com>
2018-03-01 13:40:31 -0800

    More documentation

--- migrations/2_deploy_dharma_contracts.js
+++ migrations/2_deploy_dharma_contracts.js
@@ -1,13 +1,8 @@
 const OWNERS = require("./OWNERS");
 
 module.exports = (deployer, network, accounts) => {
-    /*
-    Import our Permissions library, which will need to be deployed first so
-    that it can be linked to the contracts that depend on it.
-    */
-    const PermissionsLib = artifacts.require("PermissionsLib");
-
     // Import the Dharma contracts.
+    const PermissionsLib = artifacts.require("PermissionsLib");
     const DebtRegistry = artifacts.require("DebtRegistry");
     const DebtToken = artifacts.require("DebtToken");
     const DebtKernel = artifacts.require("DebtKernel");
@@ -32,13 +27,18 @@ module.exports = (deployer, network, accounts) => {
 
     const required = Math.floor(owners.length / 2);
 
+    // Deploy the MultiSigWallet with a set of owners and the number of
+    // authorizations required before a transaction can be executed.
+    deployer.deploy(MultiSigWallet, owners, required);
+
+    // Deploy our Permissions library and link our `DebtRegistry` to it.
     deployer.deploy(PermissionsLib);
     deployer.link(PermissionsLib, DebtRegistry);
+
     return deployer.deploy(DebtRegistry).then(async () => {
         await deployer.deploy(DebtToken, DebtRegistry.address);
         await deployer.deploy(TokenTransferProxy);
         await deployer.deploy(RepaymentRouter, DebtRegistry.address, TokenTransferProxy.address);
         await deployer.deploy(DebtKernel, TokenTransferProxy.address);
-        await deployer.deploy(MultiSigWallet, owners, required);
     });
 };
