4067ea3c0357304bbb713060f137a6f73e3352a0
Kayvon Tehranian <ktehranian@gmail.com>
2018-03-02 13:50:53 -0800

    #MultiSigWallet

---
+++ test/ts/migrations/2_deploy_dharma_contracts_test.ts
@@ -0,0 +1,43 @@
+import * as chai from "chai";
+import * as Units from "../test_utils/units";
+import { BigNumber } from "bignumber.js";
+import * as Web3 from "web3";
+import * as _ from "lodash";
+
+import { BigNumberSetup } from "../test_utils/bignumber_setup";
+import ChaiSetup from "../test_utils/chai_setup";
+
+// Set up Chai
+ChaiSetup.configure();
+const expect = chai.expect;
+
+// Configure BigNumber exponentiation
+BigNumberSetup.configure();
+
+// Dharma Contracts
+import { MultiSigWalletContract } from "../../../types/generated/multi_sig_wallet";
+
+contract("Migration #2: Deploying Dharma Contracts", async (ACCOUNTS) => {
+    const requiredComputed = new BigNumber(ACCOUNTS.length / 2);
+
+    const CONTRACT_OWNER = ACCOUNTS[0];
+    const TX_DEFAULTS = { from: CONTRACT_OWNER, gas: 4000000 };
+
+    describe("#MultiSigWallet", () => {
+        let wallet: MultiSigWalletContract;
+
+        before(async () => {
+            wallet = await MultiSigWalletContract.deployed(web3, TX_DEFAULTS);
+        });
+
+        it("lists the correct accounts as owner", async () => {
+            _.forEach(ACCOUNTS, (value: any, i: number) => {
+                expect(wallet.isOwner.callAsync(value)).to.eventually.be.true;
+            });
+        });
+
+        it("lists the correct number of required authorizations", async () => {
+            expect(wallet.required.callAsync()).to.eventually.bignumber.equal(requiredComputed);
+        });
+    });
+});
