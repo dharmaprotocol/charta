0a469dd8ffe55ec1cc3b17abfb0d505f5a21e0fc
Kayvon Tehranian <ktehranian@gmail.com>
2018-03-02 14:34:27 -0800

    #RepaymentRouter

--- test/ts/migrations/2_deploy_dharma_contracts_test.ts
+++ test/ts/migrations/2_deploy_dharma_contracts_test.ts
@@ -18,6 +18,8 @@ BigNumberSetup.configure();
 // import { MultiSigWalletContract } from "../../../types/generated/multi_sig_wallet";
 import { DebtTokenContract } from "../../../types/generated/debt_token";
 import { DebtRegistryContract } from "../../../types/generated/debt_registry";
+import { RepaymentRouterContract } from "../../../types/generated/repayment_router";
+import { TokenTransferProxyContract } from "../../../types/generated/token_transfer_proxy";
 
 contract("Migration #2: Deploying Dharma Contracts", async (ACCOUNTS) => {
     const CONTRACT_OWNER = ACCOUNTS[0];
@@ -26,16 +28,31 @@ contract("Migration #2: Deploying Dharma Contracts", async (ACCOUNTS) => {
     // let wallet: MultiSigWalletContract;
     let debtToken: DebtTokenContract;
     let debtRegistry: DebtRegistryContract;
+    let tokenTransferProxy: TokenTransferProxyContract;
+    let repaymentRouter: RepaymentRouterContract;
 
     before(async () => {
         // wallet = await MultiSigWalletContract.deployed(web3, TX_DEFAULTS);
         debtToken = await DebtTokenContract.deployed(web3, TX_DEFAULTS);
         debtRegistry = await DebtRegistryContract.deployed(web3, TX_DEFAULTS);
+        repaymentRouter = await RepaymentRouterContract.deployed(web3, TX_DEFAULTS);
+        tokenTransferProxy = await TokenTransferProxyContract.deployed(web3, TX_DEFAULTS);
     });
 
     describe("#DebtToken", () => {
-        it("maps to the deployed instance of the debt registry", async () => {
+        it("references the deployed instance of the debt registry", async () => {
             expect(debtToken.registry.callAsync()).to.eventually.equal(debtRegistry.address);
+        });
+    });
+
+    describe("#RepaymentRouter", () => {
+        it("references the deployed instances of the debt registry and token transfer proxy", async () => {
+            expect(repaymentRouter.debtRegistry.callAsync()).to.eventually.equal(
+                debtRegistry.address,
+            );
+            expect(repaymentRouter.tokenTransferProxy.callAsync()).to.eventually.equal(
+                tokenTransferProxy.address,
+            );
         });
     });
 
