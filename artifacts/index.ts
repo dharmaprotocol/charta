203db80098232097bb8a9bc21a582df3bc1fcffc
Nadav Hollander <nadav@dharma.io>
2018-03-27 00:30:03 -0700

    fix: artifacts/index.ts imports from nonexistant TermsContractRegistry

--- artifacts/index.ts
+++ artifacts/index.ts
@@ -4,7 +4,6 @@ import { RepaymentRouter } from "./ts/RepaymentRouter";
 import { TokenTransferProxy } from "./ts/TokenTransferProxy";
 import { TokenRegistry } from "./ts/TokenRegistry";
 import { DebtToken } from "./ts/DebtToken";
-import { TermsContractRegistry } from "./ts/TermsContractRegistry";
 import { DummyToken } from "./ts/DummyToken";
 import { ERC20 } from "./ts/ERC20";
 import { SimpleInterestTermsContract } from "./ts/SimpleInterestTermsContract";
@@ -18,7 +17,6 @@ export {
     TokenTransferProxy,
     TokenRegistry,
     DebtToken,
-    TermsContractRegistry,
     DummyToken,
     ERC20,
     SimpleInterestTermsContract,
