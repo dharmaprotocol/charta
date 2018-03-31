7a9727a578822afa09533c4dbb8107147b271ba9
t4sk <tsk.nakamura@gmail.com>
2018-03-31 01:11:17 -0700

    test: add test util to send transaction
    Add test util to send transaction to an overloaded method of a contract

---
+++ test/ts/test_utils/send_transactions.ts
@@ -0,0 +1,43 @@
+// Web3 types can be found here:
+// https://github.com/0xProject/0x-monorepo/tree/development/packages/types
+
+import * as Web3 from "web3";
+import ethjsABI = require("ethjs-abi");
+import { TxData } from "../../../types/common";
+
+function filterMethodABI(abi: any[]): Web3.MethodAbi[] {
+    return abi.filter((abiDef) => abiDef.type === "function");
+}
+
+function findMethod(abi: Web3.AbiDefinition[], name: string, inputTypes: string): Web3.MethodAbi {
+    const methodAbi = filterMethodABI(abi).find((abiDef) => {
+        const methodArgs = abiDef.inputs.map((input) => input.type).join(",");
+        return abiDef.name === name && methodArgs === inputTypes;
+    });
+
+    if (methodAbi) {
+        return methodAbi;
+    }
+
+    throw new Error(`Method: ${name} with input types: ${inputTypes} is not found`);
+}
+
+// sendTransaction is a util function to send a transaction to any overloaded
+// method of a contract.
+// Truffle contract instance cannot handle overloaded methods
+// (truffle will only handle the first implementation of the method).
+export default function sendTransaction(
+    truffleContractInstance: any,
+    methodName: string,
+    inputTypes: string,
+    inputVals: any[],
+    txData: TxData = {},
+): Promise<string> {
+    const abiMethod = findMethod(truffleContractInstance.abi, methodName, inputTypes);
+    const encodedData = ethjsABI.encodeMethod(abiMethod, inputVals);
+
+    return truffleContractInstance.sendTransaction({
+        data: encodedData,
+        ...txData,
+    });
+}
