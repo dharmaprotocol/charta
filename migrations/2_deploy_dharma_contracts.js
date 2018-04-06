74ba16117b7ec0645ab65c254f2c01eb986a1ecd
Graeme Boy <graemelearn@protonmail.com>
2018-04-05 18:32:44 -0700

    fix: fix migrations

--- migrations/2_deploy_dharma_contracts.js
+++ migrations/2_deploy_dharma_contracts.js
@@ -39,13 +39,5 @@ module.exports = (deployer, network, accounts) => {
         await deployer.deploy(RepaymentRouter, DebtRegistry.address, TokenTransferProxy.address);
         await deployer.deploy(DebtKernel, TokenTransferProxy.address);
         await deployer.deploy(TokenRegistry);
-
-        await deployer.deploy(
-            Collateralizer,
-            DebtKernel.address,
-            DebtRegistry.address,
-            TokenRegistry.address,
-            TokenTransferProxy.address,
-        )
     });
 };
