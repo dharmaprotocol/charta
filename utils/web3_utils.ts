93e82938f9d30c16329629db7f20cb2e56844a28
Nadav Hollander <nadav@dharma.io>
2018-04-25 16:29:25 -0700

    feat: add increaseTime to web3_utils

--- utils/web3_utils.ts
+++ utils/web3_utils.ts
@@ -15,4 +15,45 @@ export class Web3Utils {
         const codeIsEmpty = /^0x0{0,40}$/i.test(code);
         return !codeIsEmpty;
     }
+
+    /**
+     * Increases block time by the given number of seconds. Returns true
+     * if the next block was mined successfully after increasing time.
+     *
+     * @param {number} seconds
+     * @returns {Promise<boolean>}
+     */
+    public async increaseTime(seconds: number): Promise<boolean> {
+        const increaseTimeResponse = await this.sendJsonRpcRequestAsync("evm_increaseTime", [
+            seconds,
+        ]);
+
+        // A new block must be mined to make this effective.
+        const blockMineResponse = await this.mineBlock();
+
+        return !increaseTimeResponse["error"] && !blockMineResponse["error"];
+    }
+
+    /**
+     * Mines a single block.
+     *
+     * @returns {Promise<"web3".Web3.JSONRPCResponsePayload>}
+     */
+    public async mineBlock(): Promise<Web3.JSONRPCResponsePayload> {
+        return this.sendJsonRpcRequestAsync("evm_mine", []);
+    }
+
+    private async sendJsonRpcRequestAsync(
+        method: string,
+        params: any[],
+    ): Promise<Web3.JSONRPCResponsePayload> {
+        return promisify(this.web3.currentProvider.sendAsync, {
+            context: this.web3.currentProvider,
+        })({
+            jsonrpc: "2.0",
+            method,
+            params,
+            id: new Date().getTime(),
+        });
+    }
 }
