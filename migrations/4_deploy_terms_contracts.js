1089bdde73d0a72cfb49dfc315c2292d72c387a0
Kayvon Tehranian <ktehranian@gmail.com>
2018-05-03 11:21:34 -0700

    refactor: deploy terms contract in single deploy call

--- migrations/4_deploy_terms_contracts.js
+++ migrations/4_deploy_terms_contracts.js
@@ -9,39 +9,23 @@ module.exports = (deployer, network, accounts) => {
     );
     const IncompatibleTermsContract = artifacts.require("IncompatibleTermsContract");
     const Collateralizer = artifacts.require("Collateralizer");
-    const TokenTransferProxy = artifacts.require("TokenTransferProxy");
 
-    const TX_DEFAULTS = { from: accounts[0], gas: 4000000 };
-
-    deployer.deploy(IncompatibleTermsContract);
-
-    deployer.deploy(
-        SimpleInterestTermsContract,
-        DebtKernel.address,
-        DebtRegistry.address,
-        TokenRegistry.address,
-        RepaymentRouter.address
-    );
-
-    deployer.deploy(
-        Collateralizer,
-        DebtKernel.address,
-        DebtRegistry.address,
-        TokenRegistry.address,
-        TokenTransferProxy.address,
-    ).then(async () => {
-        await deployer.deploy(
+    return deployer.deploy(
+        IncompatibleTermsContract,
+        [
+            SimpleInterestTermsContract,
+            DebtKernel.address,
+            DebtRegistry.address,
+            TokenRegistry.address,
+            RepaymentRouter.address,
+        ],
+        [
             CollateralizedSimpleInterestTermsContract,
             DebtKernel.address,
             DebtRegistry.address,
             TokenRegistry.address,
             RepaymentRouter.address,
             Collateralizer.address,
-        );
-    });
-
-    // TODO: De-prioritizing Compound Interest Terms Contract for testnet beta
-
-    // deployer.deploy(CompoundInterestTermsContract);
-    // deployer.deploy(CollateralizedCompoundInterestTermsContract);
+        ],
+    );
 };
