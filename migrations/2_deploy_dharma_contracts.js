81795bc053add11ded94acf84b3ac2cdca7e1b41
Kayvon Tehranian <ktehranian@gmail.com>
2018-05-04 17:03:25 -0700

    fix: remove awaiting in main deploy block

--- migrations/2_deploy_dharma_contracts.js
+++ migrations/2_deploy_dharma_contracts.js
@@ -25,48 +25,34 @@ module.exports = (deployer, network, accounts) => {
     // Deploy the DharmaMultiSigWallet with a set of signatories, the number of
     // authorizations required before a transaction can be executed, and the
     // timelock period, defined in seconds.
-    await deployer.deploy(DharmaMultiSigWallet, signatories, numAuthorizationsRequired, timelock);
+    deployer.deploy(DharmaMultiSigWallet, signatories, numAuthorizationsRequired, timelock);
 
     // Deploy our Permissions library and link it to the contracts in our protocol that depend on it.
-    await deployer.deploy(PermissionsLib);
+    deployer.deploy(PermissionsLib);
     deployer.link(PermissionsLib, [DebtRegistry, TokenTransferProxy, Collateralizer, DebtToken]);
 
-    await deployer.deploy(DebtRegistry);
-    await deployer.deploy(DebtToken, DebtRegistry.address);
-    await deployer.deploy(TokenTransferProxy);
-    await deployer.deploy(RepaymentRouter, DebtRegistry.address, TokenTransferProxy.address);
-    await deployer.deploy(DebtKernel, TokenTransferProxy.address);
-    await deployer.deploy(
-        Collateralizer,
-        DebtKernel.address,
-        DebtRegistry.address,
-        TokenRegistry.address,
-        TokenTransferProxy.address,
-    );
-    await deployer.deploy(TokenRegistry).then(async () => {
-        if (network === CONSTANTS.LIVE_NETWORK_ID) {
-            const tokenRegistry = await TokenRegistry.deployed();
-
-            // Set the address of the tokens in the token registry.
-            await Promise.all(
-                CONSTANTS.TOKEN_LIST.map(async (token) => {
-                    const { symbol, address, decimals, name } = token;
-
-                    return tokenRegistry.setTokenAttributes(symbol, address, name, decimals, {
-                        from: OWNER,
-                    });
-                }),
-            );
-        }
+    return deployer.deploy(DebtRegistry).then(async () => {
+        await deployer.deploy(DebtToken, DebtRegistry.address);
+        await deployer.deploy(TokenTransferProxy);
+        await deployer.deploy(RepaymentRouter, DebtRegistry.address, TokenTransferProxy.address);
+        await deployer.deploy(DebtKernel, TokenTransferProxy.address);
+        await deployer.deploy(
+            Collateralizer,
+            DebtRegistry.address,
+            TokenRegistry.address,
+            TokenTransferProxy.address,
+        );
+        await deployer.deploy(TokenRegistry).then(async () => {
+        });
+        await deployer.deploy(
+            ContractRegistry,
+            Collateralizer.address,
+            DebtKernel.address,
+            DebtRegistry.address,
+            DebtToken.address,
+            RepaymentRouter.address,
+            TokenRegistry.address,
+            TokenTransferProxy.address,
+        );
     });
-    await deployer.deploy(
-        ContractRegistry,
-        Collateralizer.address,
-        DebtKernel.address,
-        DebtRegistry.address,
-        DebtToken.address,
-        RepaymentRouter.address,
-        TokenRegistry.address,
-        TokenTransferProxy.address,
-    );
 };
