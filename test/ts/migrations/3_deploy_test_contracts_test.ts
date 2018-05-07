62b4cf18bde908f856b6efdcbea456c53d265305
Kayvon Tehranian <ktehranian@gmail.com>
2018-05-06 19:00:46 -0700

    fix: add missing tests, and remove outdated tests

--- test/ts/migrations/3_deploy_test_contracts_test.ts
+++ test/ts/migrations/3_deploy_test_contracts_test.ts
@@ -14,79 +14,106 @@ const web3Utils = new Web3Utils(web3);
 BigNumberSetup.configure();
 
 // Import our set of test Contracts
-import { DummyContractContract } from "../../../types/generated/dummy_contract";
+import { DummyContractContract as DummyContract } from "../../../types/generated/dummy_contract";
+import { DummyTokenContract } from "../../../types/generated/dummy_token";
 import { MockDebtRegistryContract } from "../../../types/generated/mock_debt_registry";
 import { MockERC20TokenContract } from "../../../types/generated/mock_e_r_c20_token";
 import { MockERC721TokenContract } from "../../../types/generated/mock_e_r_c721_token";
+import { MockERC721ReceiverContract } from "../../../types/generated/mock_e_r_c721_receiver";
 import { MockDebtTokenContract } from "../../../types/generated/mock_debt_token";
-import { MockTermsContractContract } from "../../../types/generated/mock_terms_contract";
+import { MockTermsContractContract as MockTermsContract } from "../../../types/generated/mock_terms_contract";
+import { MockCollateralizedTermsContractContract as MockCollateralizedTermsContract } from "../../../types/generated/mock_collateralized_terms_contract";
+import { MockTokenRegistryContract } from "../../../types/generated/mock_token_registry";
 import { MockTokenTransferProxyContract } from "../../../types/generated/mock_token_transfer_proxy";
-
-import { TokenRegistryContract } from "../../../types/generated/token_registry";
 
 contract("Migration #3: Deploying Test Contracts", async (ACCOUNTS) => {
     const CONTRACT_OWNER = ACCOUNTS[0];
     const TX_DEFAULTS = { from: CONTRACT_OWNER, gas: 4000000 };
 
-    let dummyContract: DummyContractContract;
+    let dummyContract: DummyContract;
+    let dummyToken: DummyTokenContract;
     let mockDebtRegistry: MockDebtRegistryContract;
     let mockERC20Token: MockERC20TokenContract;
     let mockERC721Token: MockERC721TokenContract;
-    let mockDebtTokenContract: MockDebtTokenContract;
-    let mockTermsContract: MockTermsContractContract;
+    let mockERC721Receiver: MockERC721ReceiverContract;
+    let mockDebtToken: MockDebtTokenContract;
+    let mockTermsContract: MockTermsContract;
+    let mockCollateralizedTermsContract: MockCollateralizedTermsContract;
+    let mockTokenRegistry: MockTokenRegistryContract;
     let mockTokenTransferProxy: MockTokenTransferProxyContract;
-    let tokenRegistry: TokenRegistryContract;
+
+    before(async () => {
+        dummyContract = await DummyContract.deployed(web3, TX_DEFAULTS);
+        dummyToken = await DummyTokenContract.deployed(web3, TX_DEFAULTS);
+        mockDebtRegistry = await MockDebtRegistryContract.deployed(web3, TX_DEFAULTS);
+        mockERC20Token = await MockERC20TokenContract.deployed(web3, TX_DEFAULTS);
+        mockERC721Token = await MockERC721TokenContract.deployed(web3, TX_DEFAULTS);
+        mockERC721Receiver = await MockERC721ReceiverContract.deployed(web3, TX_DEFAULTS);
+        mockDebtToken = await MockDebtTokenContract.deployed(web3, TX_DEFAULTS);
+        mockTermsContract = await MockTermsContract.deployed(web3, TX_DEFAULTS);
+        mockCollateralizedTermsContract = await MockCollateralizedTermsContract.deployed(
+            web3,
+            TX_DEFAULTS,
+        );
+        mockTokenRegistry = await MockTokenRegistryContract.deployed(web3, TX_DEFAULTS);
+        mockTokenTransferProxy = await MockTokenTransferProxyContract.deployed(web3, TX_DEFAULTS);
+    });
 
     describe("Deployment", () => {
         it("should deploy the `DummyContract` contract to the current network", async () => {
-            dummyContract = await DummyContractContract.deployed(web3, TX_DEFAULTS);
-            expect(web3Utils.doesContractExistAtAddressAsync(dummyContract.address)).to.eventually
-                .be.true;
+            await expect(web3Utils.doesContractExistAtAddressAsync(dummyContract.address)).to
+                .eventually.be.true;
+        });
+
+        it("should deploy the `DummyTokenContract` contract to the current network", async () => {
+            await expect(web3Utils.doesContractExistAtAddressAsync(dummyToken.address)).to
+                .eventually.be.true;
         });
 
         it("should deploy the `MockDebtRegistry` contract to the current network", async () => {
-            mockDebtRegistry = await MockDebtRegistryContract.deployed(web3, TX_DEFAULTS);
-            expect(web3Utils.doesContractExistAtAddressAsync(mockDebtRegistry.address)).to
+            await expect(web3Utils.doesContractExistAtAddressAsync(mockDebtRegistry.address)).to
                 .eventually.be.true;
         });
 
         it("should deploy the `MockERC20Token` contract to the current network", async () => {
-            mockERC20Token = await MockERC20TokenContract.deployed(web3, TX_DEFAULTS);
-            expect(web3Utils.doesContractExistAtAddressAsync(mockERC20Token.address)).to.eventually
-                .be.true;
+            await expect(web3Utils.doesContractExistAtAddressAsync(mockERC20Token.address)).to
+                .eventually.be.true;
         });
 
         it("should deploy the `MockERC721Token` contract to the current network", async () => {
-            mockERC721Token = await MockERC721TokenContract.deployed(web3, TX_DEFAULTS);
-            expect(web3Utils.doesContractExistAtAddressAsync(mockERC721Token.address)).to.eventually
-                .be.true;
+            await expect(web3Utils.doesContractExistAtAddressAsync(mockERC721Token.address)).to
+                .eventually.be.true;
+        });
+
+        it("should deploy the `MockERC721Receiver` contract to the current network", async () => {
+            await expect(web3Utils.doesContractExistAtAddressAsync(mockERC721Receiver.address)).to
+                .eventually.be.true;
         });
 
         it("should deploy the `MockDebtToken` contract to the current network", async () => {
-            mockDebtTokenContract = await MockDebtTokenContract.deployed(web3, TX_DEFAULTS);
-            expect(web3Utils.doesContractExistAtAddressAsync(mockDebtTokenContract.address)).to
+            await expect(web3Utils.doesContractExistAtAddressAsync(mockDebtToken.address)).to
                 .eventually.be.true;
         });
 
         it("should deploy the `MockTermsContract` contract to the current network", async () => {
-            mockTermsContract = await MockTermsContractContract.deployed(web3, TX_DEFAULTS);
-            expect(web3Utils.doesContractExistAtAddressAsync(mockDebtTokenContract.address)).to
+            await expect(web3Utils.doesContractExistAtAddressAsync(mockTermsContract.address)).to
+                .eventually.be.true;
+        });
+
+        it("should deploy the `MockCollateralizedTermsContract` contract to the current network", async () => {
+            await expect(
+                web3Utils.doesContractExistAtAddressAsync(mockCollateralizedTermsContract.address),
+            ).to.eventually.be.true;
+        });
+
+        it("should deploy the `MockTokenRegistry` contract to the current network", async () => {
+            await expect(web3Utils.doesContractExistAtAddressAsync(mockTokenRegistry.address)).to
                 .eventually.be.true;
         });
 
         it("should deploy the `MockTokenTransferProxy` contract to the current network", async () => {
-            mockTokenTransferProxy = await MockTokenTransferProxyContract.deployed(
-                web3,
-                TX_DEFAULTS,
-            );
-            expect(web3Utils.doesContractExistAtAddressAsync(mockTokenTransferProxy.address)).to
-                .eventually.be.true;
-        });
-
-        it("should deploy the `TokenRegistry` contract to the current network", async () => {
-            tokenRegistry = await TokenRegistryContract.deployed(web3, TX_DEFAULTS);
-            expect(web3Utils.doesContractExistAtAddressAsync(tokenRegistry.address)).to.eventually
-                .be.true;
+            await expect(web3Utils.doesContractExistAtAddressAsync(mockTokenTransferProxy.address))
+                .to.eventually.be.true;
         });
     });
 });
