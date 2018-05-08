cb0273b371abdddbfcf7267e7d8235a2d3b758c6
Kayvon Tehranian <ktehranian@gmail.com>
2018-05-07 17:40:33 -0700

    refactor: make use of new log util funtionality in contract registry event mocks

--- test/ts/logs/contract_registry.ts
+++ test/ts/logs/contract_registry.ts
@@ -1,20 +1,25 @@
-import { Address, UInt } from "../../../types/common";
+import { Address, SolidityType } from "../../../types/common";
+import { BigNumber } from "bignumber.js";
 import * as ABIDecoder from "abi-decoder";
 import * as LogUtils from "./log_utils";
 
+export enum EventNames {
+    ContractAddressUpdated = "ContractAddressUpdated",
+}
+
 export function ContractAddressUpdated(
     contract: Address,
-    contractType: UInt,
+    contractType: BigNumber,
     oldAddress: Address,
     newAddress: Address,
 ): ABIDecoder.DecodedLog {
     return {
         address: contract,
-        events: LogUtils.getParams([
-            ["contractType", contractType],
-            ["oldAddress", oldAddress],
-            ["newAddress", newAddress],
-        ]),
-        name: "ContractAddressUpdated",
+        events: [
+            LogUtils.generateParam("contractType", SolidityType.uint8, contractType),
+            LogUtils.generateParam("oldAddress", SolidityType.address, oldAddress),
+            LogUtils.generateParam("newAddress", SolidityType.address, newAddress),
+        ],
+        name: EventNames.ContractAddressUpdated,
     };
 }
