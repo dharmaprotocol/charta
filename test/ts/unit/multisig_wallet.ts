ca5bd3031e4f13773c5c622c31f05f5e97904dbf
Kayvon Tehranian <ktehranian@gmail.com>
2018-05-21 20:11:46 -0400

    refactor: remove unncessary imports

--- test/ts/unit/multisig_wallet.ts
+++ test/ts/unit/multisig_wallet.ts
@@ -1,22 +1,16 @@
 // External
-import * as ABIDecoder from "abi-decoder";
 import { BigNumber } from "bignumber.js";
 import * as chai from "chai";
-import * as _ from "lodash";
 import * as Web3 from "web3";
 import * as Units from "../test_utils/units";
 
 // Contracts
 import { MultiSigWalletContract } from "../../../types/generated/multi_sig_wallet";
 
-// Types
-import { Address, TxData } from "../../../types/common";
-
 // Test utils
 import { BigNumberSetup } from "../test_utils/bignumber_setup";
 import ChaiSetup from "../test_utils/chai_setup";
 import { INVALID_OPCODE, REVERT_ERROR } from "../test_utils/constants";
-import { sendTransaction } from "../test_utils/send_transactions";
 
 // Configure BigNumber exponentiation
 BigNumberSetup.configure();
