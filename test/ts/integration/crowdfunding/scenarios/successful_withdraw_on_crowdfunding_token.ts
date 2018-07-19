e67fdf28ce28af22b81f367da44e6f6e9a0c6b04
Chris Min <chris@dharma.io>
2018-07-19 15:24:43 -0700

    feat: introduce more withdrawOnCrowdfungingToken scenarios

--- test/ts/integration/crowdfunding/scenarios/successful_withdraw_on_crowdfunding_token.ts
+++ test/ts/integration/crowdfunding/scenarios/successful_withdraw_on_crowdfunding_token.ts
@@ -1,3 +1,6 @@
+import { BigNumber } from "bignumber.js";
+import * as Units from "../../../test_utils/units";
+
 // Scenarios
 import {
     DEFAULT_WITHDRAW_ON_CROWDFUNDING_TOKEN_ARGS,
@@ -6,7 +9,23 @@ import {
 
 export const SUCCESSFUL_WITHDRAW_ON_CROWDFUNDING_TOKEN_SCENARIOS: WithdrawOnCrowdfundingTokenScenario[] = [
     {
-        description: "when a crowdfunding token is withdrawn on",
         ...DEFAULT_WITHDRAW_ON_CROWDFUNDING_TOKEN_ARGS,
+        description: "when there is a single repayment and a single token holder",
+    },
+    {
+        ...DEFAULT_WITHDRAW_ON_CROWDFUNDING_TOKEN_ARGS,
+        description: "when there is a single repayment and multiple token holders",
+        tokenDistribution: [new BigNumber(900), new BigNumber(600), new BigNumber(300)],
+    },
+    {
+        ...DEFAULT_WITHDRAW_ON_CROWDFUNDING_TOKEN_ARGS,
+        description: "when there are multiple repayments and a single token holder",
+        repaymentAmounts: [Units.ether(0.25), Units.ether(0.25), Units.ether(0.25)],
+    },
+    {
+        ...DEFAULT_WITHDRAW_ON_CROWDFUNDING_TOKEN_ARGS,
+        description: "when there are multiple repayments and multiple token holders",
+        repaymentAmounts: [Units.ether(0.3), Units.ether(0.3)],
+        tokenDistribution: [new BigNumber(900), new BigNumber(600), new BigNumber(300)],
     },
 ];
