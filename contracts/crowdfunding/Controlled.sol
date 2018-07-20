8b4555f2c06799c31dcfa9f1fde2f7208e19eac1
Chris Min <chris@dharma.io>
2018-07-20 14:03:25 -0700

    fix: credit MinimeToken for Controlled contract

--- contracts/crowdfunding/Controlled.sol
+++ contracts/crowdfunding/Controlled.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.18;
 
+/// Taken from Giveth's MiniMeToken: https://github.com/Giveth/minime
 contract Controlled {
     /// @notice The address of the controller is the only address that can call
     ///  a function with this modifier
