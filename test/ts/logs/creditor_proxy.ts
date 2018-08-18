1293eaa0320873bbea78df9691d0548f35b4acf9
Bo Henderson <bohende@gmail.com>
2018-08-18 21:42:21 +0530

    test: init creditor proxy test logs

--- test/ts/logs/creditor_proxy.ts
+++ test/ts/logs/creditor_proxy.ts
@@ -1,8 +1,36 @@
 import { Address, Bytes32, UInt } from "../../../types/common";
 
 import * as ABIDecoder from "abi-decoder";
-import { BigNumber } from "bignumber.js";
 import * as LogUtils from "./log_utils";
+
+export function LogCreditOrderFilled(
+    contract: Address,
+    creditor: Address,
+    creditorNonce: Uint,
+    agreementId: Bytes32,
+): ABIDecoder.DecodedLog {
+    return {
+        address: contract,
+        events: LogUtils.getParams([
+            ["_creditor", creditor],
+            ["_creditorNonce", creditorNonce],
+            ["_agreementId", agreementId],
+        ]),
+        name: "LogCreditOrderFilled",
+    };
+}
+
+export function LogCreditOrderCancelled(
+    contract: Address,
+    creditor: Address,
+    creditorNonce: Uint,
+): ABIDecoder.DecodedLog {
+    return {
+        address: contract,
+        events: LogUtils.getParams([["_creditor", creditor], ["_creditorNonce", creditorNonce]]),
+        name: "LogCreditOrderCancelled",
+    };
+}
 
 export function LogError(contract: Address, errorId: UInt): ABIDecoder.DecodedLog {
     return {
