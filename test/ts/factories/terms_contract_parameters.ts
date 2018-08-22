e3273af1c0105f83682862e5690e3ed8353b5ddf
Graeme <graeme@dharma.io>
2018-08-22 10:15:24 -0700

    Flag for enumerable (#148)
    * feat: add flag for isEnumerable
    
    * refactor: rename tokenIndex to tokenRef and fix a test
    
    * feat: use token index to get token id in retreive params
    
    * feat: conditionally use index or id

--- test/ts/factories/terms_contract_parameters.ts
+++ test/ts/factories/terms_contract_parameters.ts
@@ -15,8 +15,10 @@ export interface CollateralizedContractTerms {
 }
 
 export interface ERC721CollateralizedContractTerms {
+    isEnumerable: BigNumber;
     erc721ContractIndex: BigNumber;
-    tokenIndex: BigNumber;
+    // Can be an ID or an index.
+    tokenReference: BigNumber;
 }
 
 class TermsContractParameters {
@@ -98,13 +100,15 @@ export class ERC721CollateralizedSimpleInterestTermsParameters extends TermsCont
         // Optionally, get the full contract terms parameters string by providing the contract terms.
         contractTerms?: SimpleInterestContractTerms,
     ): string {
+        const encodedIsEnumerable = collateralTerms.isEnumerable.toString(16);
         const encodedCollateralToken = collateralTerms.erc721ContractIndex
             .toString(16)
-            .padStart(13, "0");
+            .padStart(12, "0");
 
-        const encodedTokenIndex = collateralTerms.tokenIndex.toString(16).padStart(14, "0");
+        const encodedTokenIndex = collateralTerms.tokenReference.toString(16).padStart(14, "0");
 
-        const packedCollateralParameters = encodedCollateralToken + encodedTokenIndex;
+        const packedCollateralParameters =
+            encodedIsEnumerable + encodedCollateralToken + encodedTokenIndex;
 
         if (contractTerms) {
             const packedTermsParameters = SimpleInterestParameters.pack(contractTerms);
