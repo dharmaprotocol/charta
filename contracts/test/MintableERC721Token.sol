64cc3b4f1e00dd29df38b4f2fb2d4b20b039b924
Graeme <graemelearn@protonmail.com>
2018-08-23 18:46:32 -0700

    docs: add code doc for test contract

--- contracts/test/MintableERC721Token.sol
+++ contracts/test/MintableERC721Token.sol
@@ -3,7 +3,10 @@ pragma solidity 0.4.18;
 import "zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 import "../ERC165.sol";
 
-
+/**
+ * This implementation of the ERC721 standard allows us to deploy a test ERC721 contract and mint
+ * non-fungible tokens that we can use in a test environment.
+ */
 contract MintableERC721Token is ERC721Token, ERC165 {
     function MintableERC721Token() public ERC721Token("MintableERC721Token", "MET") {}
 
