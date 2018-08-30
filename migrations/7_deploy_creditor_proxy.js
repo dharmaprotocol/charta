aac68ea36aac3f27bbbbb170e8e8817fe77b094a
shivani <gupta.shivani254@gmail.com>
2018-08-30 13:07:26 +0530

    migrations: start to fix migrations

--- migrations/7_deploy_creditor_proxy.js
+++ migrations/7_deploy_creditor_proxy.js
@@ -1,16 +1,22 @@
-const CONSTANTS = require("./migration_constants");
 const { generateParamsForDharmaMultiSigWallet, configureTokenRegistry } = require("./utils");
 
 module.exports = (deployer, network, accounts) => {
+    const DharmaMultiSigWallet = artifacts.require("DharmaMultiSigWallet");
+
     const ContractRegistry = artifacts.require("ContractRegistry");
     const CreditorProxy = artifacts.require("CreditorProxy");
     const TokenTransferProxy = artifacts.require("TokenTransferProxy");
 
-    await deployer.deploy(CreditorProxy, ContractRegistry.address);
+    // const TX_DEFAULTS = { from: accounts[0], gas: 4000000 };
 
-    const creditorProxy = await CreditorProxy.deployed();
-    const proxy = await TokenTransferProxy.deployed();
+    return deployer.then(async () => {
+        const proxy = await TokenTransferProxy.deployed();
+        const wallet = await DharmaMultiSigWallet.deployed();
 
-    // Authorize creditor proxy to make `transferFrom` calls on the token transfer proxy.
-    await proxy.addAuthorizedTransferAgent(creditorProxy.address);
+        await deployer.deploy(CreditorProxy, ContractRegistry.address);
+
+        const creditorProxy = await CreditorProxy.deployed();
+        // Authorize creditor proxy to make `transferFrom` calls on the token transfer proxy.
+        //await proxy.addAuthorizedTransferAgent(creditorProxy.address, { from: wallet.address });
+    });
 }
