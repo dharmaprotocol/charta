e28d3804adbd2c03273e3df2fef92470988ca95e
Chris Min <chris@dharma.io>
2018-10-03 17:55:03 -0700

    build: add Creditor Proxy Decision Engine deployment to migrations

--- migrations/9_deploy_ltv_decision_engine.js
+++ migrations/9_deploy_ltv_decision_engine.js
@@ -1,7 +1,9 @@
 module.exports = (deployer, network, accounts) => {
+    const CreditorProxyDecisionEngine = artifacts.require("CreditorProxyDecisionEngine");
     const LTVDecisionEngine = artifacts.require("LTVDecisionEngine");
 
     return deployer.then(async () => {
+        await deployer.deploy(CreditorProxyDecisionEngine);
         await deployer.deploy(LTVDecisionEngine);
     });
 };
